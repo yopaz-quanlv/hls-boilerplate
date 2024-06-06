f62ff751a5554fd52ba13375d571b8724513b984
thdeathz <buidunga112@gmail.com>
2024-06-06 14:57:08 +0700

    feat: change upload video button

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@ import VideosList from '~/components/video/videos-list.vue'
         :to="`/upload`"
         class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100"
       >
-        Upload
+        Upload new video
       </NuxtLink>
     </div>
 
